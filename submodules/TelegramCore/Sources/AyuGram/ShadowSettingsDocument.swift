8ddabcdbfbf0659edb51d3d9d63a8a0cacd29b51
Folzy <87141318+folzy1092@users.noreply.github.com>
2026-09-15 18:55:46 +0300

    Restore settings data after camera rollback

--- submodules/TelegramCore/Sources/AyuGram/ShadowSettingsDocument.swift
+++ submodules/TelegramCore/Sources/AyuGram/ShadowSettingsDocument.swift
@@ -34,7 +34,7 @@ public struct ShadowSettingsDocument: Codable, Equatable {
         "doubleTapToEdit", "showExactLastSeen", "showExactLastSeenSeconds",
         "wideChannelPosts", "showExactViewCounts", "showForwardCount", "preferUsernameForNonContacts", "preferUsernameForBots",
         "hideAllChatsFolder", "disableStoryCameraSwipe", "foldersAtBottom", "hideBottomSearch", "compactBottomBar",
-        "allowSaveRestrictedContent", "roundVideoUseBackCamera", "showCameraTile",
+        "allowSaveRestrictedContent", "roundVideoUseBackCamera", "roundVideoUltraWide", "customVideoMessageSpeed", "showCameraTile",
         "cameraTileLivePreview", "confirmCalls", "saveDestructingMedia",
         "saveAllIncomingMedia", "mediaAutoCleanKeepPinned", "mediaAutoCleanKeepChannels",
         "mediaAutoCleanKeepBots", "showProfileId", "showProfileDC",
